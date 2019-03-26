0dfe047d24e99c34133135b8e1e23da27a0a8359
tmhognestad <tonemarie_97@hotmail.com>
2019-03-26 17:47:51 +0100

    teest

--- sources_1/new/fsm.vhd
+++ sources_1/new/fsm.vhd
@@ -3,7 +3,11 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use IEEE.NUMERIC_STD.ALL;
 
 entity fsm is
---  Port ( );
+  Port (
+            clk, reset: in std_logic;
+            ins: in std_logic;
+            load: out std_logic
+         );
 end fsm;
 
 architecture arch of fsm is
@@ -26,6 +30,7 @@ constant HALT:  std_logic_vector(7 downto 0):= x"AF";
 
 type state is
             (
+                ALL_0, ALL_1,
                 TMS0_2, TMS1_2, 
                 RST_2, NTCK_2, SHF_2, SCFCP_2, 
                 SSO00_2, SSO01_2, SSO10_2, SSO11_2,
@@ -33,8 +38,43 @@ type state is
                 HALT_2
             );
 
+signal state_reg: state;
+signal state_next: state:= ALL_0;
+
+
+
 begin
 
+    -- Register
+    process (clk, reset)
+    begin
+        if (reset = '1') then
+            state_reg <= ALL_0;
+            load <= '0';  
+        elsif (rising_edge(clk)) then
+            state_reg <= state_next;         
+        end if;
+    end process;
+    
+    
+    -- Next state logic
+    process(state_reg)
+        begin
+        
+        case(state_reg) is
+        
+        when ALL_0 =>
+            
+        
+        when ALL_1 =>
+        
+        
+        
+        
+        end case;
+        
+        end
+    process;
 
 
 
--- sources_1/new/ins_reg.vhd
+++ sources_1/new/ins_reg.vhd
@@ -4,15 +4,25 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity ins_reg is
   Port  (
-            db: in std_logic_vector(7 downto 0);
-            clk, reset: in std_logic;
-            ins: out std_logic
+            db:     in std_logic_vector(7 downto 0);
+            clk:    in std_logic;
+            load:   in std_logic;
+            ins:    out std_logic_vector(7 downto 0)
         );
 end ins_reg;
 
 architecture arch of ins_reg is
-
+    
+    signal s_ins: std_logic_vector(7 downto 0);
+    
 begin
-
-
+    
+    process (clk, s_ins, load)
+    begin
+        if (rising_edge(clk) and load = '1') then
+            ins <= s_ins;            
+        end if;
+    end process;    
+    
+    s_ins <= db;
 end arch;
