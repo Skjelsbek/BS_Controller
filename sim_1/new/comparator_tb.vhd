e14d04eb08e47ab24a156fec1fb3c5d15e464bfd
Jon Skjelsbæk <skjelsbek@hotmail.com>
2019-03-28 12:57:25 +0100

    Instruction register and rom tested and working.

--- sim_1/new/comparator_tb.vhd
+++ sim_1/new/comparator_tb.vhd
@@ -2,11 +2,11 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 --use IEEE.NUMERIC_STD.ALL;
 
-entity deserializer_tb is
+entity comparator_tb is
 --  Port ( );
-end deserializer_tb;
+end comparator_tb;
 
-architecture arch of deserializer_tb is
+architecture arch of comparator_tb is
 
 begin
 
--- sources_1/new/bs_controller.vhd
+++ sources_1/new/bs_controller.vhd
@@ -2,21 +2,53 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity bs_controller is
---  Port ( );
+    port
+    (
+        load: in std_logic;        
+        ab: in std_logic_vector(2 downto 0);
+        ins: out std_logic_vector(7 downto 0)   
+    );
 end bs_controller;
 
-architecture Behavioral of bs_controller is
+architecture arch of bs_controller is        
+    
+    component rom
+        port
+        (
+            ab: in std_logic_vector(2 downto 0);
+            db: out std_logic_vector(7 downto 0)
+    
+        );
+    end component rom;
+    
+    component ins_reg
+        port
+        (       
+            load:   in std_logic;
+            db:     in std_logic_vector(7 downto 0);                    
+            ins:    out std_logic_vector(7 downto 0)
+        );
+    end component ins_reg;
     
     signal s_ab: std_logic_vector(2 downto 0);
     signal s_db: std_logic_vector(7 downto 0);
     
 begin
 
-    rom_c: entity work.rom(arch)
+    c_rom: rom
         port map
         (
             ab => s_ab,
             db => s_db
         );
-
-end Behavioral;
+    
+    c_ins_reg: ins_reg
+        port map
+        (
+            load => load,
+            db => s_db,
+            ins => ins
+        );
+        
+    s_ab <= ab;
+end arch;
